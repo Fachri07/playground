22b4e20af975070da0d784d7b3957eabe23ddd91
Fachri Ramadhan <85064964+Fachri07@users.noreply.github.com>
2022-03-22 14:27:42 +0700

    Update exercise-3.js
    Add answer

--- frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-3.js
+++ frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-3.js
@@ -1,3 +1,9 @@
 // Buatlah pseudocode untuk menjumlahkan bilangan genap yang ada di antara 1 sampai 10
 
 // TODO: answer here
+// START
+// FOR index FROM 1 TO 10
+//    IF index MOD 2 == 0 THEN
+//      PRINT index
+//    ENDIF
+//  ENDLOOP
